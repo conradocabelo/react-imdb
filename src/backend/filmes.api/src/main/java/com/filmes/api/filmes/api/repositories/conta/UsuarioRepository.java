7d2be849984bdda377f88b7632a65d2c5b7bdb1c
conras <bassoconradocabelo@gmail.com>
2023-05-12 07:20:40 -0300

    Alterado estrtutura do projeto

--- src/backend/filmes.api/src/main/java/com/filmes/api/filmes/api/repositories/conta/UsuarioRepository.java
+++
@@ -1,9 +0,0 @@
-package com.filmes.api.filmes.api.repositories.conta;
-
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-import com.filmes.api.filmes.api.domain.conta.Usuario;
-
-public interface UsuarioRepository extends MongoRepository<Usuario, String> {
-    Usuario findUsuarioByEmail(String email);
-}
--- src/backend/filmes.api/src/main/java/com/filmes/api/filmes/api/services/conta/UsuarioService.java
+++
@@ -1,9 +0,0 @@
-package com.filmes.api.filmes.api.services.conta;
-
-import com.filmes.api.filmes.api.domain.conta.Usuario;
-
-public interface UsuarioService {
-    Usuario buscarUsuarioPorEmail(String email);
-
-    Usuario novoUsuario(Usuario usuario) throws Exception;
-}
